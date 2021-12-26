0f663c2e46dd922786365428d434907a9e552963
Isaac Silverstein <logo4poop@protonmail.com>
2021-12-25 21:18:45 -0500

    Mostly finished the Vm itself, start work on Cli emulator

---
+++ src/cli/Cli.hx
@@ -0,0 +1,91 @@
+package cli;
+
+import vm.Vm;
+
+class Cli {
+	static var devSystem: Device;
+	static var devConsole: Device;
+
+	static function error(message: String, error: String): Bool {
+		Sys.stderr().writeString('Error $message: $error\n');
+		return false;
+	}
+
+	static function inspect(stack: Stack, name: String): Void {
+		var x: Int = 0;
+		var y: Int = 0;
+		Sys.stderr().writeString('\n$name\n');
+		
+		while (y < 0x04) {
+			while (x < 0x08) {
+				var p = y * 0x08 + x;
+				Sys.stderr().writeString('${stack.data[p]}');
+				x++;
+			}
+			Sys.stderr().writeString("\n");
+			y++;
+		}
+	}
+
+	static function nullDeviceInput(device: Device, port: Int): Int {
+		return device.data[port];
+	}
+
+	static function nullDeviceOutput(device: Device, port: Int): Void {
+		if (port == 0x1) {
+			device.vector = Vm.peek16(device.data, 0x0);
+		}
+	}
+
+	static function systemDeviceInput(device: Device, port: Int): Int {
+		switch (port) {
+			case 0x2:
+				return device.uxn.wst.ptr;
+			case 0x3:
+				return device.uxn.rst.ptr;
+			default:
+				return device.data[port];
+		}
+	}
+
+	static function systemDeviceOutput(device: Device, port: Int): Void {
+		switch(port) {
+			case 0x2:
+				device.uxn.wst.ptr = device.data[port];
+			case 0x3:
+				device.uxn.rst.ptr = device.data[port];
+			case 0xe:
+				inspect(device.uxn.wst, "Working-stack");
+				inspect(device.uxn.rst, "Return-stack");
+		}
+	}
+
+	static function main() {
+		var uxn: Uxn;
+		var i: Int = 0;
+		var loaded: Int = 0;
+	
+		// System
+		var devSystem = Vm.port(uxn, 0x0, systemDeviceInput, systemDeviceOutput);
+		// Console
+		var devconsole = Vm.port(uxn, 0x1, nullDeviceInput, consoleDeviceOutput);
+		// Empty 
+		Vm.port(uxn, 0x2, nullDeviceInput, nullDeviceOutput);
+		Vm.port(uxn, 0x3, nullDeviceInput, nullDeviceOutput);
+		Vm.port(uxn, 0x4, nullDeviceInput, nullDeviceOutput);
+		Vm.port(uxn, 0x5, nullDeviceInput, nullDeviceOutput);
+		Vm.port(uxn, 0x6, nullDeviceInput, nullDeviceOutput);
+		Vm.port(uxn, 0x7, nullDeviceInput, nullDeviceOutput);
+		Vm.port(uxn, 0x8, nullDeviceInput, nullDeviceOutput);
+		Vm.port(uxn, 0x9, nullDeviceInput, nullDeviceOutput);
+		// File
+		Vm.port(uxn, 0xa, nullDeviceInput, fileDeviceOutput);
+		// Date Time
+		Vm.port(uxn, 0xb, datetimeDeviceInput, nullDeviceOutput);
+		// Empty
+		Vm.port(uxn, 0xc, nullDeviceInput, nullDeviceOutput);
+		Vm.port(uxn, 0xd, nullDeviceInput, nullDeviceOutput);
+		Vm.port(uxn, 0xe, nullDeviceInput, nullDeviceOutput);
+		Vm.port(uxn, 0xf, nullDeviceInput, nullDeviceOutput);
+	}
+}
--- src/vm/Vm.hx
+++ src/vm/Vm.hx
@@ -8,13 +8,14 @@ typedef Stack = {
 }
 
 typedef Device = {
+	uxn: Uxn,
 	stack: Stack,
 	address: Int,
 	data: Array<Int>,
-	memory: Int,
+	memory: Array<Int>,
 	vector: Int,
 	input: (d:Device, i:Int)->Int,
-	output: (d:Device, i:Int)->Int
+	output: (d:Device, i:Int)->Void
 }
 
 typedef Memory = {
@@ -49,7 +50,7 @@ class Vm {
 	static var halt: (uxn: Uxn, error: Int, name: String, id: Int)->Bool;
 
 	// Byte Mode
-	static function push8(stack: Stack, value: Int): Void {
+	public static function push8(stack: Stack, value: Int): Void {
 		if (stack.ptr == 0xff) {
 			stack.error = 2;
 			return;
@@ -57,8 +58,7 @@ class Vm {
 		stack.data[stack.ptr++] = value;
 	}
 
-	// Removes the last value 
-	static function pop8k(stack: Stack): Int {
+	public static function pop8k(stack: Stack): Int {
 		if (stack.kptr == 0) {
 			stack.error = 1;
 			return 0;
@@ -66,7 +66,7 @@ class Vm {
 		return stack.data[stack.kptr--];
 	}
 
-	static function pop8d(stack: Stack): Int {
+	public static function pop8d(stack: Stack): Int {
 		if (stack.ptr == 0) {
 			stack.error = 1;
 			return 0;
@@ -74,71 +74,71 @@ class Vm {
 		return stack.data[stack.ptr--];
 	}
 
-	static function poke8(memory: Array<Int>, a: Int, b: Int): Void {
+	public static function poke8(memory: Array<Int>, a: Int, b: Int): Void {
 		memory[a] = b;
 	}
 
-	static function peek8(memory: Array<Int>, a: Int): Int {
+	public static function peek8(memory: Array<Int>, a: Int): Int {
 		return memory[a];
 	}
 
-	static function devw8(device: Device, a: Int, b: Int): Void {
+	public static function devw8(device: Device, a: Int, b: Int): Void {
 		device.data[a & 0xf] = b;
 		device.output(device, a & 0x0f);
 	}
 
-	static function devr8(device: Device, a: Int): Int {
-		return device.output(device, a & 0x0f);
+	public static function devr8(device: Device, a: Int): Int {
+		return device.input(device, a & 0x0f);
 	}
 
-	static function warp8(uxn: Uxn, a: Int): Void {
+	public static function warp8(uxn: Uxn, a: Int): Void {
 		uxn.memory.ptr += a;
 	}
 	
-	static function pull8(uxn: Uxn): Void {
+	public static function pull8(uxn: Uxn): Void {
 		push8(uxn.src, peek8(uxn.memory.data, uxn.memory.ptr++));
 	}
 
 	// Short Mode
-	static function push16(stack: Stack, a: Int): Void {
+	public static function push16(stack: Stack, a: Int): Void {
 		push8(stack, a >> 8);
 		push8(stack, a);
 	}
 
-	static function pop16(stack: Stack): Int {
+	public static function pop16(stack: Stack): Int {
 		var a: Int = pop8(stack);
 		var b: Int = pop8(stack);
 		return a + (b << 8);
 	}
 
-	static function poke16(memory: Array<Int>, a: Int, b: Int): Void {
+	public static function poke16(memory: Array<Int>, a: Int, b: Int): Void {
 		poke8(memory, a, b >> 8);
 		poke8(memory, a + 1, b);
 	}
 
-	static function peek16(memory: Array<Int>, a: Int): Int {
+	public static function peek16(memory: Array<Int>, a: Int): Int {
 		return (peek8(memory, a) << 8) + peek8(memory, a + 1);
 	}
 
-	static function devw16(device: Device, a: Int, b: Int): Void {
+	public static function devw16(device: Device, a: Int, b: Int): Void {
 		devw8(device, a, b >> 8);
 		devw8(device, a + 1, b);
 	}
-
-	static function devr16(device: Device, a: Int): Int {
+	
+	public static function devr16(device: Device, a: Int): Int {
 		return (devr8(device, a) << 8) + devr8(device, a + 1);
 	}
 	
-	static function warp16(uxn: Uxn, a: Int): Void {
+	public static function warp16(uxn: Uxn, a: Int): Void {
 		uxn.memory.ptr = a;
 	}
 
-	static function pull16(uxn: Uxn): Void {
+	public static function pull16(uxn: Uxn): Void {
 		push16(uxn.src, peek16(uxn.memory.data, uxn.memory.ptr++));
 		uxn.memory.ptr++;
 	}
 
-	static public function eval(uxn: Uxn, vector: Int): Bool {
+	public static function eval(uxn: Uxn, vector: Int): Bool {
 		var instr: Int;
 		var a: Int;
 		var b: Int;
@@ -197,39 +197,39 @@ class Vm {
 
 			switch (instr & 0x1f) {
 				// Stack Opcodes
-				// LIT
+				// LIT: Pushes the next value seen in the program onto the stack
 				case 0x00:
 					pull(uxn);
 					break;
-				// INC
+				// INC: Adds 1 to the value at the top of the stack
 				case 0x01:
 					a = pop(uxn.src);
 					push(uxn.src, a + 1);
 					break;
-				// POP
+				// POP: Removes the value at the top of the stack
 				case 0x02:
 					pop(uxn.src);
 					break;
-				// DUP
+				// DUP: Duplicates the value at the top of the stack
 				case 0x03:
 					a = pop(uxn.src);
 					push(uxn.src, a);
 					push(uxn.src, a);
 					break;
-				// NIP
+				// NIP: Removes the second value from the stack
 				case 0x04:
 					a = pop(uxn.src);
 					pop(uxn.src);
 					push(uxn.src, a);
 					break;
-				// SWP
+				// SWP: Exchanges the first and second values at the top of the stack
 				case 0x05:
 					a = pop(uxn.src);
 					b = pop(uxn.src);
 					push(uxn.src, a);
 					push(uxn.src, b);
 					break;
-				// OVR
+				// OVR: Duplicates the second value at the top of the stack
 				case 0x06:
 					a = pop(uxn.src);
 					b = pop(uxn.src);
@@ -237,7 +237,7 @@ class Vm {
 					push(uxn.src, a);
 					push(uxn.src, b);
 					break;
-				// ROT
+				// ROT: Rotates three values at the top of the stack, to the left, wrapping around
 				case 0x07:
 					a = pop(uxn.src);
 					b = pop(uxn.src);
@@ -247,81 +247,81 @@ class Vm {
 					push(uxn.src, c);
 					break;
 				// Logic Opcodes
-				// EQU
+				// EQU: Pushes 01 to the stack if the two values at the top of the stack are equal, 00 otherwise
 				case 0x08:
 					a = pop(uxn.src);
 					b = pop(uxn.src);
 					push8(uxn.src, b == a ? 1 : 0);
 					break;
-				// NEQ
+				// NEQ: Pushes 01 to the stack if the two values at the top of the stack are not equal, 00 otherwise
 				case 0x09:
 					a = pop(uxn.src);
 					b = pop(uxn.src);
 					push8(uxn.src, b != a ? 1 : 0);
 					break;
-				// GTH
+				// GTH: Pushes 01 to the stack if the second value at the top of the stack is greater than the value at the top of the stack, 00 otherwise
 				case 0x0a:
 					a = pop(uxn.src);
 					b = pop(uxn.src);
 					push8(uxn.src, b > a ? 1 : 0);
 					break;
-				// LTH
+				// LTH: Pushes 01 to the stack if the second value at the top of the stack is lesser than the value at the top of the stack, 00 otherwise
 				case 0x0b:
 					a = pop(uxn.src);
 					b = pop(uxn.src);
 					push8(uxn.src, b < a ? 1 : 0);
 					break;
-				// JMP
+				// JMP: Moves the program counter by a signed value equal to the byte on the top of the stack, or an absolute address in short mode
 				case 0x0c:
 					a = pop(uxn.src);
 					warp(uxn, a);
 					break;
-				// JCN
+				// JCN: If the byte preceeding the address is not 00, moves the program counter by a signed value equal to the byte on the top of the stack, or an absolute address in short mode
 				case 0x0d:
 					a = pop(uxn.src);
 					if (pop8(uxn.src) > 0) {
 						warp(uxn, a);
 					}
 					break;
-				// JSR
+				// JSR: Pushes the value of the program counter to the return-stack and moves the program counter by a signed value equal to the byte on the top of the stack, or an absolute address in short mode
 				case 0x0e:
 					a = pop(uxn.src);
 					push16(uxn.dst, uxn.memory.ptr);
 					warp(uxn, a);
 					break;
-				// STH
+				// STH: Moves the value at the top of the stack, to the return stack
 				case 0x0f:
 					a = pop(uxn.src);
 					push(uxn.dst, a);
 					break;
 				// Memory Opcodes
-				// LDZ
+				// LDZ: Pushes the value at an address within the first 256 bytes of memory, to the top of the stack
 				case 0x10:
 					a = pop8(uxn.src);
 					push(uxn.src, peek(uxn.memory.data, a));
 					break;
-				// STZ
+				// STZ: Writes a value to an address within the first 256 bytes of memory
 				case 0x11:
 					a = pop8(uxn.src);
 					b = pop(uxn.src);
 					poke(uxn.memory.data, a, b);
 					break;
-				// LDR
+				// LDR: Pushes the value at a relative address, to the top of the stack. The possible relative range is -128 to +127 bytes
 				case 0x12:
 					a = pop8(uxn.src);
 					push(uxn.src, peek(uxn.memory.data, uxn.memory.ptr + a));
 					break;
-				// STR
+				// STR: Writes a value to a relative address. The possible relative range is -128 to +127 bytes
 				case 0x13:
 					a = pop8(uxn.src);
 					b = pop(uxn.src);
 					poke(uxn.memory.data, uxn.memory.ptr + a, b);
-				// LDA
+				// LDA: Pushes the value at a absolute address, to the top of the stack
 				case 0x14:
 					a = pop16(uxn.src);
 					push(uxn.src, peek(uxn.memory.data, a));
 					break;
-				// STA
+				// STA: Writes a value to a absolute address
 				case 0x15:
 					a = pop16(uxn.src);
 					b = pop(uxn.src);
@@ -339,50 +339,50 @@ class Vm {
 					devw(uxn.device[a >> 4], a, b);
 					break;
 				// Arithmetic Opcodes
-				// ADD
+				// ADD: Pushes the sum of the two values at the top of the stack
 				case 0x18:
 					a = pop(uxn.src);
 					b = pop(uxn.src);
 					push(uxn.src, Math.round(b + a));
 					break;
-				// SUB
+				// SUB: Pushes the difference of the first value minus the second, to the top of the stack
 				case 0x19:
 					a = pop(uxn.src);
 					b = pop(uxn.src);
 					push(uxn.src, Math.round(b - a));
 					break;
-				// MUL
+				// MUL: Pushes the product of the first and second values at the top of the stack
 				case 0x1a:
 					a = pop(uxn.src);
 					b = pop(uxn.src);
 					push(uxn.src, Math.round(b * a));
 					break;
-				// DIV
+				// DIV: Pushes the quotient of the first value over the second, to the top of the stack
 				case 0x1b:
 					a = pop(uxn.src);
 					b = pop(uxn.src);
 					push(uxn.src, Math.round(b / a));
 					break;
-				// AND
+				// AND: Pushes the result of the bitwise operation AND, to the top of the stack
 				case 0x1c:
 					a = pop(uxn.src);
 					b = pop(uxn.src);
 					push(uxn.src, b & a);
 					break;
-				// ORA
+				// ORA: Pushes the result of the bitwise operation OR, to the top of the stack
 				case 0x1d:
 					a = pop(uxn.src);
 					b = pop(uxn.src);
 					push(uxn.src, b | a);
 					break;
-				// EOR
+				// EOR: Pushes the result of the bitwise operation XOR, to the top of the stack
 				case 0x1e:
 					a = pop(uxn.src);
 					b = pop(uxn.src);
 					push(uxn.src, b ^ a);
 					break;
-				// SFT
-				case 0x1d:
+				// SFT (0x1d): Moves the bits of the value at the top of the stack to the left or right, depending on the control value of the second. The high nibble of the control value determines how many bits left to shift, and the low nibble how many bits right to shift. The rightward shift is done first
+				default:
 					a = pop(uxn.src);
 					b = pop(uxn.src);
 					push(uxn.src, b >> (a & 0x0f) << ((a & 0xf0) >> 4));
@@ -398,18 +398,12 @@ class Vm {
 		return true;
 	}
 
-	/*
-	static function boot(uxn: Uxn): Bool {
-		var i: Int = 0;
-		for (ul in uxn.length) {
-			i++;
-			uxn[ul] = 0x00;
-		}
-		return true;
-	}
-	*/
-	
-	static function main() {
-		trace("Hello World!");
+	public static function port(uxn: Uxn, id: Int, deifn: (device: Device, port: Int)->Int, deofn: (device: Device, port: Int)->Void): Device {
+		var device: Device = uxn.device[id];
+		device.address = id * 0x10;
+		device.memory = uxn.memory.data;
+		device.input = deifn;
+		device.output = deofn;
+		return device;
 	}
 }
